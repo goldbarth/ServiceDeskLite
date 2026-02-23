e8ed93f2c35b0ebad2177055449d08e5ed32b368
Felix Wahl <47465895+goldbarth@users.noreply.github.com>
2026-02-23 10:29:31 +0100

    fix(docs): close navbar on page load in viewport

---
+++ docs/template/styles/main.js
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// Close navbar when page loads.
+// Without this, the Bootstrap navbar opens automatically when the page loads
+// because the CSS class “in” is set incorrectly.
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.getElementById("navbar");
+    if (navbar) {
+        navbar.classList.remove("in");
+    }
+});
